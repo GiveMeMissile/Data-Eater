c885240dd6ab2dc7bfc683a2f41649dc1fb9d28d
Dylan <givememissiles@gmail.com>
2026-03-18 19:43:25 -0500

    Added functionality to both parser.py and navigator.py files, with them currently desined to be able to get information from a test website, this will change later on as the project expands

---
+++ navigator.py
@@ -0,0 +1,18 @@
+class Navigator:
+    # Class which uses Playwright in order to navigate the internet and get html data to be used by the parser
+
+    def __init__(self, page):
+        self.page = page
+        self.original_url = page.url()
+
+    # Function which uses playwright to access static html
+    async def get_html(self):
+        await self.page.wait_for_load_state("domcontentloaded")
+        html = await self.page.content()
+        return html
+    
+    async def goto(self, url):
+        await self.page.goto(url)
+
+    async def return_to_original(self):
+        await self.page.goto(self.original_url)
---
+++ parser.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+class Parser:
+    # Class which contain tools which will parse messy HTML into organized data.
+    def __init__(self):
+        pass
+
+    def parse_through_quotes(self, html):
+        soup = BeautifulSoup(html, "lxml")
+        quotes_html = soup.find_all("div", class_="quote")
+        quotes = []
+        for quote_html in quotes_html:
+            quote_text = quote_html.find("span", class_="text").text
+            quote_author = quote_html.find("small", class_="author").text
+            print(quote_author)
+            quotes.append((quote_text, quote_author))
+        next_url = soup.find("li", class_="next").a["href"]
+        return quotes, next_url
