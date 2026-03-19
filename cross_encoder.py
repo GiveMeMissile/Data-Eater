4efd364899e861868c4e44a808015ce9736917ac
Dylan <givememissiles@gmail.com>
2026-03-19 11:30:51 -0500

    Added Cross Encoding

---
+++ cross_encoder.py
@@ -0,0 +1,26 @@
+from sentence_transformers import CrossEncoder
+
+MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
+
+class CrossEncoding:
+    # A more advanced filter used to determine te similarity between two texts.
+    # Will be used to pick out the best texts picked from the bi encoder. 
+    # Acts as a final filter before the data is saved into the dataset. 
+
+    def __init__(self, texts):
+        self.model = CrossEncoder(MODEL)
+        self.texts = texts
+
+    def compare(self, text):
+        # Compares the inputted text with the saved texts, outputs a list of the comparisons between the texts. 
+
+        comparison_results = []
+
+        for c_text in self.texts:
+            comparison_results.append(self.model.predict((text, c_text)))
+
+        return comparison_results
+    
+    def direct_compare(self, t1, t2):
+        # Directly compares two inputted texts
+        return self.model.predict((t1, t2))
